a71b709ee0603df8b160137b9293d8a45c3347db
Franciele Barros <franbarros@ibm.com>
2021-06-09 16:32:51 -0300

    add implementação de segurança centralizada

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const httpProxy = require('express-http-proxy');
+const auth = require('./security/auth');
 
 const express = require('express');
 const app = express();
@@ -9,39 +10,44 @@ var logger = require('morgan');
 require('dotenv').config();
 app.use(logger('dev'));
 
-const router = express.Router();
-
 const HOST = process.env.API_HOST;
 const PORT = process.env.SERVER_PORT;
 
-function selectProxyHost(req) {
-    if (req.path.startsWith('/user')){
+function selectProxyHost(req, res) {
+    if (req.path.startsWith('/api/user')){
         console.log('Api Gateway is calling ms-user...');
         return HOST + ":" +process.env.MS_USER_PORT;
     }    
 
-    if (req.path.startsWith('/patient'))  {
+    if (req.path.startsWith('/api/patient'))  {
         console.log('Api Gateway is calling ms-patient...');
         return HOST + ":" + process.env.MS_PATIENT_PORT;
     }  
 
-    if (req.path.startsWith('/doctor'))  {
+    if (req.path.startsWith('/api/doctor'))  {
         console.log('Api Gateway is calling ms-doctor...');
         return HOST + ":" + process.env.MS_DOCTOR_PORT;  
     }
 
-    if (req.path.startsWith('/appointment'))  {
+    if (req.path.startsWith('/api/appointment'))  {
         console.log('Api Gateway is calling ms-appointment...');
         return HOST + ":" + process.env.MS_APPOINTMENT_PORT;  
     }
-
-    else if (req.path.startsWith('/test')){
-        //tratamento erro
+    else if (req.path.startsWith('/')){
+        res.status(404).send("Path does not exist!");
     }
 }
+//helth check
+app.get('/online', function(req, res){
+    res.status(200).send({message:'API-GATEWAY IS UP'});
+});
+
+// Authentication
+app.use('/api', auth);
+
  
-app.use((req, res, next) => {
-    httpProxy(selectProxyHost(req))(req, res, next);
+app.use((req, res, next)  => {
+    httpProxy(selectProxyHost(req, res))(req, res, next);
 });
 
 app.listen(PORT, () => {
---
+++ security/auth.js
@@ -0,0 +1,24 @@
+var jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+var naoValidarToken = function(req){
+  return req.originalUrl === '/api/user/v1/auth/login' || req.originalUrl === '/api/user/v1/auth/registrar';
+}
+function verifyToken(req, res, next) {
+
+  if(naoValidarToken(req))
+    return next();
+    
+  var token = req.headers['x-api-token'];
+  if (!token){
+    return res.status(403).send({ auth: false, message: 'Token not provided.' });
+  }
+  jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
+    if (err)
+    return res.status(500).send({ auth: false, message: 'Invalid token, try again.' });
+    // if everything good, save to request for use in other routes
+    req.userId = decoded.id;
+    next();
+  });
+}
+module.exports = verifyToken;
