71c7ddbc5eca34c09b7ed4f9c6ee80a9f2b7c4a3
Rafael Brito <rafael.souzabrito7@gmail.com>
2021-06-22 18:48:37 -0300

    conditionally check host if docker or localhost

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const httpProxy = require('express-http-proxy');
 const auth = require('./security/auth');
+const isDocker = require('is-docker');
 
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 var logger = require('morgan');
@@ -11,22 +12,36 @@ require('dotenv').config();
 app.use(logger('dev'));
 
 const HOST = process.env.API_HOST;
+const DOCTORS_HOST = process.env.DOCTORS_MS_HOST;
+const PATIENTS_HOST = process.env.PATIENTS_MS_HOST;
+const APPOINTMENTS_HOST = process.env.APPOINTMENTS_MS_HOST;
 const PORT = process.env.SERVER_PORT;
+
 
 function selectProxyHost(req, res) {
     if (req.path.startsWith('/api/patients'))  {
-        console.log('Api Gateway is calling ms-patient...');
+      console.log('Api Gateway is calling ms-patient...');
+      if (isDocker()) {
+        console.log(isDocker());
+        return PATIENTS_HOST + ":" + process.env.MS_PATIENT_PORT;
+      }
         return HOST + ":" + process.env.MS_PATIENT_PORT;
     }  
 
     if (req.path.startsWith('/api/doctors'))  {
-        console.log('Api Gateway is calling ms-doctor...');
-        return HOST + ":" + process.env.MS_DOCTOR_PORT;  
+      console.log('Api Gateway is calling ms-doctor...');
+      if(isDocker()) {
+        return DOCTORS_HOST + ":" + process.env.MS_DOCTOR_PORT;
+      }
+      return HOST + ":" + process.env.MS_DOCTOR_PORT;  
     }
 
     if (req.path.startsWith('/api/appointments'))  {
-        console.log('Api Gateway is calling ms-appointment...');
-        return HOST + ":" + process.env.MS_APPOINTMENT_PORT;  
+      console.log('Api Gateway is calling ms-appointment...');
+      if(isDocker()) {
+        return APPOINTMENTS_HOST + ":" + process.env.MS_APPOINTMENT_PORT;
+      }
+      return HOST + ":" + process.env.MS_APPOINTMENT_PORT;  
     }
     else if (req.path.startsWith('/')){
         res.status(404).send("Path does not exist!");
